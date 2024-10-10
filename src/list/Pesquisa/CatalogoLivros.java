e6bb274c6285ba78de1cf5f7bbad1577ae6d3153
Hercules Herculano <hbheroi@gmail.com>
2024-10-10 15:47:28 -0300

    criacao das classes CatalogoLivros e Livro

---
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,19 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros(List<Livro> livroList) {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo,autor,anoPublicacao));
+    }
+
+
+}

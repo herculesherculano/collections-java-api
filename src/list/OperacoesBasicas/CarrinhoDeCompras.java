cd4437b630ec185cdc07207ce80adbcf57eb2a9a
Hercules Herculano <hbheroi@gmail.com>
2024-10-10 14:37:13 -0300

    criacao das classes Item e CarrinhoDeCompras

---
+++ src/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,15 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itensList;
+
+    public CarrinhoDeCompras() {
+        this.itensList = new ArrayList<>();
+    }
+
+
+}
---
+++ src/list/OperacoesBasicas/Item.java
@@ -0,0 +1,22 @@
+package list.OperacoesBasicas;
+
+public class Item {
+
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+}

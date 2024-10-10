6fff33a3b446c2d7a73ff15b86142504f9b66f45
Hercules Herculano <hbheroi@gmail.com>
2024-10-10 17:56:04 -0300

    implementacao dos metodos pesquisarPorIntervaloAnos e pesquisarPorTitulo

--- src/list/Pesquisa/CatalogoLivros.java
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -15,26 +15,47 @@ public class CatalogoLivros {
         livroList.add(new Livro(titulo,autor,anoPublicacao));
     }
 
-    public List pesquisaPorAutor(String autor){
-        List<Livro> livrosEncontrados = new ArrayList<>();
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
         if (!livroList.isEmpty()){
             for(Livro l: livroList){
                 if(l.getAutor().equalsIgnoreCase(autor)){
-                    livrosEncontrados.add(l);
+                    livrosPorAutor.add(l);
                 }
             }
-        }else{
-            System.out.println("A lista está vazia");
         }
-        return livrosEncontrados;
+        return livrosPorAutor;
     }
 
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorInvervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()){
+            for (Livro l: livroList){
+                if(l.getAnoPublicacao()>=anoInicial && l.getAnoPublicacao()<=anoFinal){
+                    livrosPorInvervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorInvervaloAnos;
+    }
 
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo= null;
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo=l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
 
 
     public static void main(String[] args) {
         CatalogoLivros catalogoLivros = new CatalogoLivros();
-        System.out.println(catalogoLivros.pesquisaPorAutor("Autor2"));
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor2"));
         catalogoLivros.adicionarLivro("Titulo1", "Autor1", 2002);
         catalogoLivros.adicionarLivro("Titulo2", "Autor1", 2003);
         catalogoLivros.adicionarLivro("Titulo3", "Autor1", 2004);
@@ -43,9 +64,12 @@ public class CatalogoLivros {
         catalogoLivros.adicionarLivro("Titulo30", "Autor2", 2004);
         catalogoLivros.adicionarLivro("Titulo100", "Autor3", 2002);
         catalogoLivros.adicionarLivro("Titulo200", "Autor3", 2003);
-        catalogoLivros.adicionarLivro("Titulo300", "Autor3", 2004);
+        catalogoLivros.adicionarLivro("Titulo300", "Autor3", 2005);
+        catalogoLivros.adicionarLivro("Titulo1", "Autor3", 2003);
 
-        System.out.println(catalogoLivros.pesquisaPorAutor("Autor2"));
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2002,2003));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Titulo1"));
 
     }
 }

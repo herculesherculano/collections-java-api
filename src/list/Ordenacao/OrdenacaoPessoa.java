c0888b89606313f784015a833df88acf5cf7a1ca
Hercules Herculano <hbheroi@gmail.com>
2024-10-14 16:04:03 -0300

    implementacao dos metodos de ordenacao por altura e de ordenacao por idade

--- src/list/Ordenacao/OrdenacaoPessoa.java
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -1,6 +1,8 @@
 package list.Ordenacao;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoa {
@@ -15,5 +17,33 @@ public class OrdenacaoPessoa {
         pessoaList.add(new Pessoa(nome, idade, altura));
     }
 
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
 
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+    public static void main(String[] args) {
+        //instanciando a classe OrdenacaoPessoa
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        //adicionando pessoas a lista de ordenação de pessoas
+        ordenacaoPessoa.adicionarPessoa("João", 30, 1.60);
+        ordenacaoPessoa.adicionarPessoa("Maria", 28, 1.65);
+        ordenacaoPessoa.adicionarPessoa("José", 29, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Pedro", 25, 1.75);
+
+        //ordenando e exibindo a lista de pessoas por idade
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+
+        //ordenando e exibindo a lista de pessoas por altura
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+
+    }
 }
--- src/list/Ordenacao/Pessoa.java
+++ src/list/Ordenacao/Pessoa.java
@@ -1,5 +1,7 @@
 package list.Ordenacao;
 
+import java.util.Comparator;
+
 public class Pessoa implements Comparable<Pessoa> {
 
     private String nome;
@@ -38,4 +40,13 @@ public class Pessoa implements Comparable<Pessoa> {
                 '}';
     }
 
+}
+
+class ComparatorPorAltura implements Comparator<Pessoa>{
+
+
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
+    }
 }

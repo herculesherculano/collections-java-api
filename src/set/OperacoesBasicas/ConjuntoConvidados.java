e7875ea03e6d69ad4e14af80dd9fae3ce31b5e2b
Hercules Herculano <hbheroi@gmail.com>
2024-10-14 19:03:11 -0300

    implementacao dos metodos remover convidados e contar convidados

--- src/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -16,5 +16,43 @@ public class ConjuntoConvidados {
         convidadoSet.add(new Convidado(nome, codigoConvite));
     }
 
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover= null;
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
 
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("João", 1234);
+        conjuntoConvidados.adicionarConvidado("Maria", 1235);
+        conjuntoConvidados.adicionarConvidado("José", 1235);
+        conjuntoConvidados.adicionarConvidado("Pedro", 1236);
+
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+        conjuntoConvidados.exibirConvidados();
+
+    }
 }
--- src/set/OperacoesBasicas/Convidado.java
+++ src/set/OperacoesBasicas/Convidado.java
@@ -1,5 +1,7 @@
 package set.OperacoesBasicas;
 
+import java.util.Objects;
+
 public class Convidado {
 
     //atributos
@@ -18,6 +20,19 @@ public class Convidado {
 
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigoConvite());
     }
 
     @Override

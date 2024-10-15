d98bc580b1ba118c63433c452c9961727bf250c4
Hercules Herculano <hbheroi@gmail.com>
2024-10-15 01:05:08 -0300

    implementacao dos metodos adicionar, exibir, pesquisarPorNome e atualizarNumero

--- src/set/Pesquisa/AgendaContatos.java
+++ src/set/Pesquisa/AgendaContatos.java
@@ -11,5 +11,56 @@ public class AgendaContatos {
         this.contatoSet = new HashSet<>();
     }
 
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+    }
 
+    public void exibirContatos(){
+        if(!contatoSet.isEmpty()){
+            System.out.println(contatoSet);
+        }else{
+            System.out.println("A agenda está vazia!");
+        }
+    }
+
+    public Set<Contato> pesquisarContato(String nome){
+        Set<Contato> contatosPorNome=new HashSet<>();
+        for(Contato c: contatoSet){
+            if(c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for(Contato c: contatoSet){
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return  contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+       AgendaContatos agendaContatos = new AgendaContatos();
+
+       agendaContatos.exibirContatos();
+
+       agendaContatos.adicionarContato("Hercules", 123456);
+       agendaContatos.adicionarContato("Bruno", 124365);
+       agendaContatos.adicionarContato("Bruno Miguel", 132567);
+       agendaContatos.adicionarContato("João", 283491);
+
+       agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarContato("Bruno"));
+        agendaContatos.atualizarNumeroContato("Bruno", 986473);
+
+        agendaContatos.exibirContatos();
+
+    }
 }
--- src/set/Pesquisa/Contato.java
+++ src/set/Pesquisa/Contato.java
@@ -1,5 +1,7 @@
 package set.Pesquisa;
 
+import java.util.Objects;
+
 public class Contato {
 
     private String nome;
@@ -16,6 +18,23 @@ public class Contato {
 
     public int getNumero() {
         return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getNome());
     }
 
     @Override

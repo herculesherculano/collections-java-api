3c8daffe26fc8b0c30a2124e60b818e76fbde7ca
Hercules Herculano <hbheroi@gmail.com>
2024-10-15 00:31:34 -0300

    criacao das classes Contato e AgendaContatos

---
+++ src/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,15 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+
+}
